61a8d6244616cbc56827a45f2d7c52633b5d3196
sivajiyelugoti <sivaji.y@gmail.com>
2022-02-15 04:54:32 +0000

    first commit

---
+++ src/Hello.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var string: String  = "Hello, World!"
+    println("$string")
+
+    println( 200 )
+    println( "200" )
+    println( 2 + 2 )
+
+    print(4*3)
+}
